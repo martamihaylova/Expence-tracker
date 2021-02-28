2c89439c9ab3ffb82e21d764a1dbb1611acb13f1
Marta Mihaylova <martamihaylovajs@gmail.com>
2021-02-28 22:51:51 +0200

    Set router and user pages

---
+++ controllers/homeController.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', async (req, res) => {
+    try {
+        let user = await req.user;
+        // let courses = await Courses.find().sort({ createdAt: - 1 }).lean();
+        if (user === null) {
+            console.log('null');
+            res.redirect('/');
+        }
+        if (!user) {
+            // courses.sort((a, b) => b.usersEnrolled.length - a.usersEnrolled.length);
+            res.render('home', { authenticated: req.isAuthenticated()});
+        } else {
+            res.render('account-info', {
+                authenticated: req.isAuthenticated(),
+                id: user?._id,
+                name: user?.username,
+                courses
+            })
+        }
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const routes = require('./config/routes');
 const config = require('./config/config');
 const app =  express();
 
 require('./config/express')(app);
 require('./config/mongoose');
+require('./config/routes')(app);
+app.use(routes);
 console.log(process.env.NODE_ENV);
 
 app.listen(config.PORT, console.log.bind(console, `Server listening on port ${config.PORT}...`));
