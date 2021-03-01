7c114e649359382fa2566cb16afbc7b71e2c8531
Marta Mihaylova <martamihaylovajs@gmail.com>
2021-03-01 23:12:59 +0200

    Set authentication and mongoose models

--- services/registerUser.js
+++ services/registerUser.js
@@ -2,27 +2,35 @@ const Users = require('../models/Users');
 const bcrypt = require('bcrypt');
 const { SALT_ROUNDS } = require('../config/config');
 
-function register(email, name, password, req, res) {
-    
+function register(name, password, amount, req, res) {
+    console.log(amount);
     Users.find({}, 'username')
-    .then((data) => {
-        console.log(data);
-        let found = data.find((x) => x?.username.toLowerCase() === name.toLowerCase());
-        if(found) res.render('register', { messages: { error: 'Username allready exists.Please try again.' }, title: 'Register' });
-    });
+        .then((data) => {
+            // console.log(data);
+            let found = data.find((x) => x?.username.toLowerCase() === name.toLowerCase());
+            if (found) res.render('register', { messages: { error: 'Username allready exists.Please try again.' }, title: 'Register' });
+        });
 
     bcrypt.hash(password, SALT_ROUNDS)
         .then((hashedPassword) => {
             let user = new Users({
-                email,
                 username: name,
                 password: hashedPassword,
+                amount
             });
-            user.save();
-            req.login(user, function (err) {
-                if (err) { return next(err); }
-                return res.redirect('/');
-            });
+            user.save()
+                .then(() => {
+                    console.log(user);
+                    res.locals.user = user;
+                    req.login(user, function (err) {
+                        if (err) { return next(err); }
+                        return res.redirect('/');
+                    });
+                })
+                .catch((error) => {
+                    console.log('hello');
+                    return res.render('register', { messages: { error: error.message }, title: 'Register' });
+                });
         })
         .catch((err) => {
             console.log(err.message);
