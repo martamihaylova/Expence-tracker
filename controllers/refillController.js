882a63f1c9cf69f504a6f5af153b16d63a6a5dd1
Marta Mihaylova <martamihaylovajs@gmail.com>
2021-03-07 01:45:25 +0200

    Validate refill

--- controllers/refillController.js
+++ controllers/refillController.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const refill = require('../services/refill');
+const Users = require('../models/Users');
 const check = require('../middleware/checkAuth');
 const router = Router();
 
-router.post('/:id', check.ifLoged, (req, res) => {
+router.post('/:id', check.ifLoged, async (req, res) => {
     if(Number(req.body.amount)){
         refill(req.params.id, req.body)
         .then(() => {
@@ -11,7 +12,14 @@ router.post('/:id', check.ifLoged, (req, res) => {
         })
         .catch((err) => console.log(err.message))
     } else {
-        res.render( '/', { messages: { error: 'Invalid inputs' } });
+         let user = await Users.findById(req.params.id).populate('expenses').lean();
+            res.render('expenses', {
+                authenticated: req.isAuthenticated(),
+                id: user?._id,
+                name: user?.username,
+                expenses: user.expenses,
+                messages: { error: 'Invalid input' }
+            });
     }
 });
 
