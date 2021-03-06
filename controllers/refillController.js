dce99484d70f529496c034bc038b7828c106c273
Marta Mihaylova <martamihaylovajs@gmail.com>
2021-03-07 00:53:30 +0200

    Fix validation

--- controllers/refillController.js
+++ controllers/refillController.js
@@ -3,19 +3,6 @@ const refill = require('../services/refill');
 const check = require('../middleware/checkAuth');
 const router = Router();
 
-// router.get('/:id', check.ifLoged, async (req, res) => {
-//     let user = await req.user;
-//     getOne(req.params.id)
-//         .then((currentCourse) => {
-//             res.render('edit-course', {
-//                 authenticated: req.isAuthenticated(),
-//                 currentCourse,
-//                 id: user?._id,
-//                 name: user?.username
-//             })
-//         })
-//         .catch((err) => console.log(er.message));
-// });
 router.post('/:id', check.ifLoged, (req, res) => {
     refill(req.params.id, req.body)
         .then(() => {
